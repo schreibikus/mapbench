74e00ebeddd2f0d254921a402349151ea9fedac4
Sergey Yasinsky <seregajsv@gmail.com>
2022-03-23 10:22:39 +0300

    Added use case for tracking short-lived tokens.

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # mapbench
 
-This is a simple C++ application for testing the speed of counting incoming connections.
-Everything is simple.
-Userid is present by unique number and we create a map for counting count requests from users.
+This is a simple C++ application for testing the speed of counting incoming connections and token tracking.
 
-## Results
+### Results
 
-8.37961s - STL map based on Red-Black Tree.
-2.34829s - STL unordered_map based on Hash Table.
-2.19521s - Boost unordered_map based on Hash container.
-7.00824s - My map implementaion based on AVL(Adelson-Velsky and Landis) Tree.
+| Connections | Tokens | MAP |
+| ------ | ------ |  ------ |  
+| 8.70492s | 20.9568s | STL map based on Red-Black Tree. |
+| 2.39977s | 6.05628s | STL unordered_map based on Hash Table. |
+| 2.40172s | 5.83537s | Boost unordered_map based on Hash container. |
+| 7.96565s | 28.7991s | My map implementaion based on AVL(Adelson-Velsky and Landis) Tree. |
 
 ## Conclusion
-
-In this case, when Userid values are concentrated in one small range(0-1000000), it is preferable to use an unordered_map.
-
+For this project it is preferable to use unordered_map.
--- bench.cpp
+++ bench.cpp
@@ -1,10 +1,10 @@
 #include <boost/unordered/unordered_map.hpp>
-#include <boost/bimap.hpp>
 #include <unordered_map>
 #include <iostream>
 #include <cxxabi.h>
 #include <cstdlib>
 #include <chrono>
+#include <list>
 #include <map>
 
 class ClientInfo
@@ -22,6 +22,7 @@ class MyMap
     using const_Key = const Key;
     using value_type = std::pair<const_Key, Value>;
 public:
+    using key_type = Key;
     class Node{
         public:
             value_type pair_obj;
@@ -144,6 +145,53 @@ private:
         }
         return 0;
     }
+
+    Node *get_tree_min(Node *head) {
+        return head->left ? get_tree_min(head->left) : head;
+    }
+
+    Node *remove_tree_min(Node *head) {
+        Node **node = &head;
+        if(head->left == 0)
+            return head->right;
+        head->left = remove_tree_min(head->left);
+        balance(node);
+        return node ? *node : 0;
+    }
+
+    Node *join_tree(Node *l, Node *r) {
+        Node *node = 0;
+        if(!r)
+            node = l;
+        else {
+            Node *minnode = get_tree_min(r);
+            minnode->right = remove_tree_min(r);
+            minnode->left = l;
+            node = minnode;
+        }
+        return node;
+    }
+
+    int remove_tree(Node **head, const_Key &obj_key) {
+        int result = -1;
+        if(head && *head) {
+            if((*head)->pair_obj.first > obj_key)
+                result = remove_tree(&(*head)->left, obj_key);
+            else if((*head)->pair_obj.first < obj_key)
+                result = remove_tree(&(*head)->right, obj_key);
+            else {
+                Node *remNode = (*head);
+                *head = join_tree((*head)->left, (*head)->right);
+                remNode->left = remNode->right = 0;
+                delete remNode;
+                result = 0;
+            }
+            if(result == 0)
+                balance(head);
+        }
+        return result;
+    }
+
 public:
     Iterator insert(const value_type &obj) {
         return Iterator(insert_tree(&root, new Node(obj)));
@@ -158,6 +206,8 @@ public:
         root = nullptr;
     }
     Iterator end() { return Iterator(nullptr); }
+
+    void erase(const Key& key) { remove_tree(&root, key); }
 };
 
 template<typename F, typename... Args>
@@ -186,7 +236,7 @@ template<typename Container>
 void clientStatistics(Container &a, unsigned int keys_range, unsigned int clientConnections)
 {
     for(decltype(clientConnections) i = 0; i < clientConnections; ++i) {
-        auto key = std::rand() % keys_range;
+        typename Container::key_type key = std::rand() % keys_range;
         auto iterator = a.find(key);
         if(iterator != a.end()) {
             iterator->second.incCon(); //increase count of requests
@@ -207,6 +257,28 @@ void clientStatistics(Container &a, unsigned int keys_range, unsigned int client
 }
 
 template<typename Container>
+void tokenAccess(Container &a, unsigned int keys_range, unsigned int clientConnections)
+{
+    std::list<typename Container::key_type> trackList;
+
+    for(decltype(clientConnections) i = 0; i < clientConnections; ++i) {
+        typename Container::key_type key = std::rand() * std::rand() * std::rand();
+        auto iterator = a.find(key);
+        if(iterator != a.end()) {
+            // token already present access denied
+        } else {
+            a.insert({key, 1});
+            trackList.push_back(key);
+        }
+        if(trackList.size() >= keys_range) {
+            key = *trackList.begin();
+            trackList.pop_front();
+            a.erase(key);
+        }
+    }
+}
+
+template<typename Container>
 void bench()
 {
     const unsigned int keys_range = 1000000;
@@ -222,6 +294,8 @@ void bench()
     std::cout << "Insert " << keys_range << " random values " << funcTime(insertMapRandom<Container>, a, keys_range) << " us" << std::endl;
     a.clear();
     std::cout << "Simulation of " << keys_range << " users witt a total of " << connections << " connections " << funcTime(clientStatistics<Container>, a, keys_range, connections) << " us" << std::endl;
+    a.clear();
+    std::cout << "Simulation of " << connections << " tokens checking " << funcTime(tokenAccess<Container>, a, keys_range, connections) << " us" << std::endl;
     a.clear();
 }
 
